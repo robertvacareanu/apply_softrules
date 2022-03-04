c00a1e4360fa9ef2db807327ee4c82f872f3d1b1
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-04 12:35:14 -0700

    Escape if needed

--- src/rulegeneration/simple_rule_generation.py
+++ src/rulegeneration/simple_rule_generation.py
@@ -2,12 +2,23 @@ from typing import Dict
 from odinson.ruleutils.queryast import *
 from odinson.ruleutils.queryparser import parse_surface
 
+def escape_if_needed(word):
+    word_copy = word
+    chars = [
+        "'",
+        '"',
+    ]
+    for char in chars:
+        if char in word:
+            word_copy = word_copy.replace(char, f'\{char}')
+
+    return word_copy
 
 def word_rule(data: Dict) -> AstNode:
     if data['e1_start'] > data['e2_start']:
-        return parse_surface(' '.join([f"""[word="\{x}"]""" for x in data['tokens'][data['e2_end']:data['e1_start']]]))
+        return parse_surface(' '.join([f"""[word="{escape_if_needed(x)}"]""" for x in data['tokens'][data['e2_end']:data['e1_start']]]))
     else:
-        return parse_surface(' '.join([f"""[word="\{x}"]""" for x in data['tokens'][data['e1_end']:data['e2_start']]]))
+        return parse_surface(' '.join([f"""[word="{escape_if_needed(x)}"]""" for x in data['tokens'][data['e1_end']:data['e2_start']]]))
 
 if __name__ == "__main__":
     data1 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', '(', 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931'}
