4876543a93b8ed9de0c8ddfd980da7a9945ad08b
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-06 13:17:58 -0700

    Argparser

---
+++ src/argparser.py
@@ -0,0 +1,7 @@
+import argparse
+
+def get_softrules_argparser():
+    parser = argparse.ArgumentParser(description='Read paths to config files (last takes precedence). Can also update parameters with command-line parameters', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--path', nargs='+', type=str, default = ["config/default_config.yaml"], help='Path(s) to config file(s)')
+
+    return parser
