435c3cbe8fc33739790acb262b1c7ff213ed7950
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-07 19:14:22 -0700

    Rule Expander Baseline

--- src/apps/eval/rule_expander_eval.py
+++ src/apps/eval/rule_expander_eval.py
@@ -2,10 +2,15 @@ import json
 from odinson.gateway.document import Document
 from odinson.ruleutils.queryparser import parse_surface
 import torch
+import glob
 import torch.nn as nn
 import tqdm 
 import hashlib
 import multiprocessing
+import concurrent
+from concurrent.futures.thread import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+from pathos.pools import ParallelPool
 
 from src.model.baseline.word_embedding_baseline import get_word_embedding
 from src.dataprocessing.tacred.dataset_converter import load_dataset_from_jsonl
@@ -35,15 +40,28 @@ def helper_function(tuple_l_c):
             result[rule[0]] += 1  
     return result
 
-def rule_expander_eval(config: Config):
-    x = 0
 
-    tacred_dataset = load_dataset_from_jsonl(config.get('dataset_path'))['train'].filter(lambda line: line['e1_start'] - line['e2_end'] != 0 and line['e2_start'] - line['e1_end'] != 0)
+def helper_function2(ee_tacred_rules):
+    ee           = ee_tacred_rules[0]
+    tacred_rules = ee_tacred_rules[1]
+
+    result       = defaultdict(int)
+    for rule in tacred_rules:
+        rule_str = rule[1]
+        # print(rule_str)
+        if ee.search(rule_str).total_hits > 0:
+            result[rule[0]] += 1  
+    return result
+
+
+
+def rule_expander_eval(config: Config):
+    tacred_dataset = load_dataset_from_jsonl(config.get_path('dataset_path'))['train'].filter(lambda line: line['e1_start'] - line['e2_end'] != 0 and line['e2_start'] - line['e1_end'] != 0)
     tacred_rules   = []
-    wre            = WordRuleExpander(config.get("faiss_index")['index_path'], config.get("faiss_index")['vocab_path'], total_random_indices=config.get("faiss_index")["total_random_indices"])
-    with open(config.get('rules_path')) as fin:
+    wre            = WordRuleExpander(config.get("faiss_index").get_path('index_path'), config.get("faiss_index").get_path('vocab_path'), total_random_indices=config.get("faiss_index").get("total_random_indices"))
+    with open(config.get_path('rules_path')) as fin:
         lines = fin.readlines()
-        for line in tqdm.tqdm(lines[:100]):
+        for line in tqdm.tqdm(lines):
             split = line.split('\t')
             rule  = parse_surface(split[0].strip())
             if len(wre.extract_words(rule)) <= config.get('max_rule_length'):
@@ -52,41 +70,32 @@ def rule_expander_eval(config: Config):
                     tacred_rules.append((split[1].strip(), e))
             tacred_rules.append((split[1].strip(), split[0].strip()))
 
-    with open(config.get('odinson_data_dir') + "/md5_to_doc_location.json") as fin:
-        md5_to_path_map = json.load(fin)
-
-    gw = OdinsonGateway.launch(javaopts=['-Xmx10g'])
 
     gold = []
     pred = []
 
+    gw = OdinsonGateway.launch(javaopts=['-Xmx10g'])
+    ee = gw.open_index(config.get_path('odinson_index_dir'))
 
-
-    data = []
-    for i, line in tqdm.tqdm(enumerate(tacred_dataset)):
-        # FIXME memory inefficient; any way for closures?
-        data.append((line, config, tacred_rules, md5_to_path_map, gw))
-        # md5      = hashlib.md5(' '.join(line['tokens']).encode('utf-8')).hexdigest()
-        # doc_path = config.get('odinson_data_dir') + '/' + md5_to_path_map[md5]
-        # doc      = Document.from_file(doc_path)
-        # ee       = gw.open_memory_index([doc])
-        # result   = defaultdict(int)
-        # for rule in tacred_rules:
-            # rule_str = rule[1]
-            # print(rule_str)
-            # if ee.search(rule_str).total_hits > 0:
-                # result[rule[0]] += 1
-
+    doc_to_rules_matched = {}
+    print("Apply each rule")
+    for tr in tqdm.tqdm(tacred_rules):
+        result = ee.search(tr[1])
+        for doc in result.docs:
+            docId = ee.extractor_engine.index().doc(doc.doc).getValues("docId")[0]
+            if docId not in doc_to_rules_matched:
+                doc_to_rules_matched[docId] = defaultdict(int)
+            doc_to_rules_matched[docId][tr[0]] += 1
     
-    pool = multiprocessing.Pool(40)
-    parallel_result = pool.map(helper_function, data)
-
+        
+    prefix = config.get('odinson_doc_file_prefix')
     for i, line in tqdm.tqdm(enumerate(tacred_dataset)):
         gold.append(line['relation'])
-        if len(parallel_result[i]) == 0:
-            pred.append('no_relation')
+        prediction = doc_to_rules_matched.get(f'{prefix}_{i}', {})
+        if len(prediction) == 0:
+            pred.append(config.get('no_relation_label'))
         else:
-            pred.append(max(parallel_result[i].items(), key=lambda x: x[1])[0])
+            pred.append(max(prediction, key=lambda x: x[1])[0])
 
     print('accuracy: ', accuracy_score(gold, pred))
     print('---------------------------------------')
@@ -98,9 +107,9 @@ def rule_expander_eval(config: Config):
     print('precision: ', precision_score(gold, pred, average="macro"))
     print('recall: ', recall_score(gold, pred, average="macro"))
 
-# python -m src.apps.eval.rule_expander_eval --path config/eval/rule_expander_baseline.yaml
+# python -m src.apps.eval.rule_expander_eval --path config/base_config.yaml config/eval/rule_expander_baseline.yaml
 if __name__ == "__main__":
-    config = Config.parse_args_and_get_config().get('rule_expander_baseline')
+    config = Config.parse_args_and_get_config()
     rule_expander_eval(config)
 
 
