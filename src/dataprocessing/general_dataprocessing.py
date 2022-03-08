7a2110c7070c6147a6e7a254e5967b2477e67250
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-08 11:18:42 -0700

    Abstract reader

--- src/dataprocessing/general_dataprocessing.py
+++ src/dataprocessing/general_dataprocessing.py
@@ -1,4 +1,8 @@
 import json
+from src.dataprocessing.tacred import load_dataset_from_jsonl as tacred_loader
+from src.dataprocessing.fewrel import load_dataset_from_jsonl as fewrel_loader
+from src.dataprocessing.semeval import load_dataset_from_jsonl as semeval_loader
+from src.dataprocessing.tacred_fewshot import load_dataset_from_jsonl as tacred_fewshot_loader
 
 def from_json_to_jsonl(from_path: str, to_path: str):
     with open(from_path) as fin:
@@ -9,6 +13,21 @@ def from_json_to_jsonl(from_path: str, to_path: str):
             fout.write(s)
             fout.write('\n')
 
+dataset_name_to_reader = {
+    'tacred'        : tacred_loader,
+    'fewrel'        : fewrel_loader,
+    'semeval'       : semeval_loader,
+    'tacred_fewshot': tacred_fewshot_loader,
+}
+
+def load_dataset_from_jsonl(path: str, dataset_name: str):
+    if dataset_name not in dataset_name_to_reader:
+        raise ValueError(f"The dataset name ({dataset_name}) is not find in the map, which contains the following keys: {list(dataset_name_to_reader.keys())}")
+    return dataset_name_to_reader[dataset_name](path)
+
 if __name__ == "__main__":
-    from_json_to_jsonl("data_sample/tacred/sample.json", 'data_sample/tacred/sample.jsonl')
-    from_json_to_jsonl("data_sample/fewrel/sample_unrolled.json", 'data_sample/fewrel/sample_unrolled.jsonl')
+    # from_json_to_jsonl("data_sample/tacred/sample.json", 'data_sample/tacred/sample.jsonl')
+    # from_json_to_jsonl("data_sample/fewrel/sample_unrolled.json", 'data_sample/fewrel/sample_unrolled.jsonl')
+    from_json_to_jsonl("/data/nlp/corpora/softrules/tacred/train/train.json", "/data/nlp/corpora/softrules/tacred/processed/train.jsonl")
+    from_json_to_jsonl("/data/nlp/corpora/softrules/tacred/dev/dev.json", "/data/nlp/corpora/softrules/tacred/processed/dev.jsonl")
+    from_json_to_jsonl("/data/nlp/corpora/softrules/tacred/test/test.json", "/data/nlp/corpora/softrules/tacred/processed/test.jsonl")
