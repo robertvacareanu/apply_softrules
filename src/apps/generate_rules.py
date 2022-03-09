c2e90a6a0a682d033adf2e567248464fd18e8d8e
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-09 14:04:24 -0700

    Using WordRuleGenerator

--- src/apps/generate_rules.py
+++ src/apps/generate_rules.py
@@ -2,19 +2,23 @@ from typing import Dict
 from src.config import Config
 import tqdm
 
-from src.rulegeneration.simple_rule_generation import word_rule
+from src.rulegeneration.simple_rule_generation import WordRuleGenerator, word_rule
 from src.dataprocessing.general_dataprocessing import load_dataset_from_jsonl
 
 
-def generate_rules(config: Dict):
+def generate_rules(config: Config):
+    wrg     = WordRuleGenerator(use_entities=config.get('use_entities'))
     dataset = load_dataset_from_jsonl(config.get_path('dataset_path'), config.get('dataset_name'))
+    d = []
+    for l in dataset[config.get('split_name')]:
+        d.append(l)
 
     with open(config.get_path('save_path'), 'w+') as fout:
-        for line in tqdm.tqdm(dataset[config.get('split_name')]):
+        for line in tqdm.tqdm(d[1805:]):
             # print(line)
             if line['e1_start'] - line['e2_end'] != 0 and line['e2_start'] - line['e1_end'] != 0 and line['relation'] != 'no_relation':
                 relation = line['relation']
-                fout.write(f'{word_rule(line)}\t{relation}\n')
+                fout.write(f'{wrg.word_rule(line).to_ast()}\t{relation}\n')
     
 # python -m src.apps.generate_rules --path config/base_config.yaml config/generate_rules.yaml
 if __name__ == "__main__":
--- src/rulegeneration/simple_rule_generation.py
+++ src/rulegeneration/simple_rule_generation.py
@@ -43,7 +43,7 @@ class Rule:
 class WordRuleGenerator:
 
     """
-    :param use_full_sentence -> whether to use the complete 
+    :param use_entities -> whether to use the complete 
     """
     def __init__(self, use_entities: bool):
         self.use_entities = use_entities
@@ -64,32 +64,11 @@ class WordRuleGenerator:
             else:
                 rule = Rule(None, tokens, None)
 
+        rule.to_ast() # just checking that it compiles; TODO better/cleaner ways?
         return rule
 
 
 
-
-"""
-Escape word if needed.
-The characters that are considered are: {', "}. Otherwise return the word as is
-"""
-def escape_if_needed(word):
-    # FIXME Maybe better ways to ensure proper escaping
-    word_copy = word
-    chars = [
-        "'",
-        '"',
-    ]
-    replaced = False
-    for char in chars:
-        if char in word:
-            word_copy = word_copy.replace(char, f'\{char}')
-            replaced = True
-    if not replaced:
-        word_copy = word_copy.encode("unicode_escape").decode("utf-8")
-    # for char in ['\\']:
-        # if char in word:
-            # word_copy = word_copy.replace(char, f'\\\\')
 
 """
 Create a rule using the words in-between the two entities
@@ -97,17 +76,18 @@ Create a rule using the words in-between the two entities
 def word_rule(data: Dict) -> AstNode:
     if data['e1_start'] > data['e2_start']:
         tokens = data['tokens'][data['e2_end']:data['e1_start']]
-        return parse_surface(' '.join([f"""[word="{escape_if_needed(x)}"]""" for x in tokens]))
+        return parse_surface(' '.join([f"""[word="{Rule.escape_if_needed(x)}"]""" for x in tokens]))
     else:
         tokens = data['tokens'][data['e1_end']:data['e2_start']]
-        return parse_surface(' '.join([f"""[word="{escape_if_needed(x)}"]""" for x in tokens]))
+        return parse_surface(' '.join([f"""[word="{Rule.escape_if_needed(x)}"]""" for x in tokens]))
 
 if __name__ == "__main__":
-    data1 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', '(', 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931'}
-    data2 = {'tokens': ['The', 'system', 'as', 'described', 'above', 'has', 'its', 'greatest', 'application', 'in', 'an', 'arrayed', 'configuration', 'of', 'antenna', 'elements', '.'], 'e1_start': 12, 'e1_end': 13, 'e2_start': 15, 'e2_end': 16, 'e1': ['configuration'], 'e2': ['elements'], 'relation': 3}
-    data3 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', '"', 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931'}
-    data4 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', "'", 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931'}
-    print(word_rule(data1))
-    print(word_rule(data2))
-    print(word_rule(data3))
-    print(word_rule(data4))
+    data1 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', '(', 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931', 'e1_type': 'entity', 'e2_type': 'entity'}
+    data2 = {'tokens': ['The', 'system', 'as', 'described', 'above', 'has', 'its', 'greatest', 'application', 'in', 'an', 'arrayed', 'configuration', 'of', 'antenna', 'elements', '.'], 'e1_start': 12, 'e1_end': 13, 'e2_start': 15, 'e2_end': 16, 'e1': ['configuration'], 'e2': ['elements'], 'relation': 3, 'e1_type': 'entity', 'e2_type': 'entity'}
+    data3 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', '"', 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931', 'e1_type': 'entity', 'e2_type': 'entity'}
+    data4 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', "'", 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931', 'e1_type': 'entity', 'e2_type': 'entity'}
+    wrg = WordRuleGenerator(True)
+    print(wrg.word_rule(data1).to_ast())
+    print(wrg.word_rule(data2).to_ast())
+    print(wrg.word_rule(data3).to_ast())
+    print(wrg.word_rule(data4).to_ast())
