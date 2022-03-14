ac6d94060342581e269a094784e37e011a84da5a
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-13 17:41:58 -0700

    More command line arguments

--- src/argparser.py
+++ src/argparser.py
@@ -9,6 +9,14 @@ def word_eval_parser(parent_parser):
     subparser.add_argument('--number-of-words-left-right', type=int)
     subparser.add_argument('--skip-unknown-words', type=bool)
     subparser.add_argument('--mode-of-application', type=str)
+    subparser.add_argument('--rules-path', type=str)
+
+    return parent_parser
+
+def dataset_details_parser(parent_parser):
+    subparser = parent_parser.add_argument_group("dataset")
+    subparser.add_argument('--dataset-path', type=str)
+    subparser.add_argument('--dataset-name', type=str)
 
     return parent_parser
 
@@ -17,13 +25,15 @@ def get_softrules_argparser():
     parser.add_argument('--path', nargs='+', type=str, default = ["config/default_config.yaml"], help='Path(s) to config file(s)')
     parser.add_argument('--basepath', type=str, required=False)
     word_eval_parser(parser)
+    dataset_details_parser(parser)
     return parser
 
 # python -m src.argparser --basepath "testbasepath" --thresholds 0.5 0.6 0.7 0.8 0.9 0.99 0.999 --use-full-sentence false --number-of-words-left-right 2 --skip-unknown-words True --mode-of-application 'apply_rules_with_threshold'
 # python -m src.argparser --basepath "testbasepath" --gensim-fname 'abc'
+# python -m src.argparser --dataset-path softrules/tacred/processed/dev.jsonl --dataset-name tacred --rules-path softrules/tacred/processed/train_rules2
 if __name__ == "__main__":
     parser = get_softrules_argparser()
-    word_eval_parser(parser)
+
     args = parser.parse_args(sys.argv[1:])
     args = vars(args)
     print(args)
