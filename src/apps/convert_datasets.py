06cd2a8ce57973635dcbc9dfc20f68e834af6785
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-14 18:57:54 -0700

    Refactor

--- src/apps/convert_datasets.py
+++
@@ -1,7 +0,0 @@
-from src.dataprocessing.general_dataprocessing import from_json_to_jsonl
-
-# python -m src.apps.convert_datasets
-def convert_datasets_to_jsonl_format():
-    from_json_to_jsonl("/data/nlp/corpora/softrules/tacred/train/train.json", "/data/nlp/corpora/softrules/tacred/processed/train.jsonl")
-    from_json_to_jsonl("/data/nlp/corpora/softrules/tacred/dev/dev.json", "/data/nlp/corpora/softrules/tacred/processed/dev.jsonl")
-    from_json_to_jsonl("/data/nlp/corpora/softrules/tacred/test/test.json", "/data/nlp/corpora/softrules/tacred/processed/test.jsonl")
