1e9d3b08c40868105827368c9a694a87e868998e
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-15 09:19:11 -0700

    Refactor
    Delete the old word_rule function from simple_rule_generation
    Now the word rule generation happens inside the WordRuleGenerator class
    This is because we want to control if to use the entities or not

--- src/rulegeneration/simple_rule_generation.py
+++ src/rulegeneration/simple_rule_generation.py
@@ -87,19 +87,6 @@ class WordRuleGenerator:
 
 
 
-
-"""
-Create a rule using the words in-between the two entities
-"""
-def word_rule(data: Dict) -> AstNode:
-    exit()
-    if data['e1_start'] > data['e2_start']:
-        tokens = data['tokens'][data['e2_end']:data['e1_start']]
-        return parse_surface(' '.join([f"""[word="{Rule.escape_if_needed(x)}"]""" for x in tokens]))
-    else:
-        tokens = data['tokens'][data['e1_end']:data['e2_start']]
-        return parse_surface(' '.join([f"""[word="{Rule.escape_if_needed(x)}"]""" for x in tokens]))
-
 if __name__ == "__main__":
     data1 = {'tokens': ['Merpati', 'flight', '106', 'departed', 'Jakarta', '(', 'CGK', ')', 'on', 'a', 'domestic', 'flight', 'to', 'Tanjung', 'Pandan', '(', 'TJQ', ')', '.'], 'e1_start': 16, 'e1_end': 17, 'e2_start': 13, 'e2_end': 15, 'e1': ['TJQ'], 'e2': ['Tanjung', 'Pandan'], 'relation': 'P931', 'e1_type': 'entity', 'e2_type': 'entity'}
     data2 = {'tokens': ['The', 'system', 'as', 'described', 'above', 'has', 'its', 'greatest', 'application', 'in', 'an', 'arrayed', 'configuration', 'of', 'antenna', 'elements', '.'], 'e1_start': 12, 'e1_end': 13, 'e2_start': 15, 'e2_end': 16, 'e1': ['configuration'], 'e2': ['elements'], 'relation': 3, 'e1_type': 'entity', 'e2_type': 'entity'}
