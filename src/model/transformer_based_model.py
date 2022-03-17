266ceba10b7fd46b8c8557aa62cc86a25ab70618
Robert Vacareanu <robert.vacareanu@gmail.com>
2022-03-16 22:58:46 -0700

    Model

---
+++ src/model/transformer_based_model.py
@@ -0,0 +1,14 @@
+import pytorch_lightning as pl
+import torch
+from torch import nn
+from transformers import BertModel, BertConfig, AdamW
+from transformers import get_cosine_schedule_with_warmup, get_linear_schedule_with_warmup
+from transformers import BertTokenizerFast
+from typing import Dict, List
+from sklearn.metrics import f1_score, precision_score, recall_score
+from collections import namedtuple
+from dataclasses import asdict, dataclass, make_dataclass
+
+class TransformerBasedScorer(pl.LightningModule):
+    def __init__(self):
+        pass
